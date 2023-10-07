68e7ecba33215428aaa1109b0c6560fa725ef9a9
apple <apple@MacBook-Pro.local>
2023-10-07 11:39:54 +0530

    New Code For Alert

---
+++ ShareVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareVideo
+//
+//  Created by apple on 07/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+}
+
