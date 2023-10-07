0df92537592f87014d4c88b2ef82e1a110bf31c1
apple <apple@MacBook-Pro.local>
2023-10-07 11:54:12 +0530

    Code New

--- ShareImage/ViewController.swift
+++ ShareImage/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn(_ sender: Any) {
+        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
