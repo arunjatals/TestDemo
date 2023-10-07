5e376e353384288bf65b337c5430eabd39393880
arunjatals <147227866+arunjatals@users.noreply.github.com>
2023-10-07 11:45:06 +0530

    Update ViewController.swift

--- ShareImage/ViewController.swift
+++ ShareImage/ViewController.swift
@@ -11,6 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.Alert)
+alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+self.presentViewController(alert, animated: true, completion: nil)
         // Do any additional setup after loading the view.
     }
 
